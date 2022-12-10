50457382491a1bc173a0dde325dc5728ba8eb978
Graze <reece2004f@gmail.com>
2022-12-10 12:42:27 +0000

    a

--- Simple-Pov/Plugin.cs
+++ Simple-Pov/Plugin.cs
@@ -5,6 +5,7 @@ using System.Collections;
 using System.IO;
 using System.Reflection;
 using UnityEngine;
+using UnityEngine.XR;
 using Utilla;
 
 namespace Simple_Pov
@@ -19,9 +20,16 @@ namespace Simple_Pov
     [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
     public class Plugin : BaseUnityPlugin
     {
-        bool set = false;
-        GameObject camank;
+        Camera cam;
         GameObject shoulderCamera;
+        GameObject cm;
+        GameObject head;
+        bool joyLC;
+        bool joyRC;
+        Vector3 ogvec;
+        
+        private readonly XRNode lNode = XRNode.LeftHand;
+        private readonly XRNode rNode = XRNode.RightHand;
         void Start()
         {
             Utilla.Events.GameInitialized += OnGameInitialized;
@@ -29,24 +37,33 @@ namespace Simple_Pov
         void OnGameInitialized(object sender, EventArgs e)
         {
             shoulderCamera = GameObject.Find("Global/Third Person Camera/Shoulder Camera");
-            GameObject.Find("Global/Third Person Camera").transform.GetChild(0).transform.GetChild(0).gameObject.SetActive(false);
-            GameObject.Find("Global/Third Person Camera").transform.GetChild(0).GetComponent<Camera>().fieldOfView = 100;
-            StartCoroutine(ExecuteAfterTime(1));
+            ogvec = shoulderCamera.transform.localPosition;
+            cm = shoulderCamera.transform.GetChild(0).gameObject;
+            cam = shoulderCamera.GetComponent<Camera>();
+            head = GameObject.Find("Global/Local VRRig/Local Gorilla Player/rig/body/head");
+            shoulderCamera.transform.SetParent(head.transform, false);
         }
-        void Update()
+        void FixedUpdate()
         {
-            if (set == true)
+            InputDevices.GetDeviceAtXRNode(lNode).TryGetFeatureValue(UnityEngine.XR.CommonUsages.primary2DAxisClick, out joyLC);
+            InputDevices.GetDeviceAtXRNode(rNode).TryGetFeatureValue(UnityEngine.XR.CommonUsages.primary2DAxisClick, out joyRC);
+
+            if (joyLC)
             {
-               
+               cm.SetActive(false);
+               cam.fieldOfView = 100;
+               shoulderCamera.transform.localPosition = new Vector3(0.0f, 0.0f, 0.0f);
+               shoulderCamera.transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+               shoulderCamera.transform.SetParent(head.transform);
+
             }
-        }
-        IEnumerator ExecuteAfterTime(float time)
-        {
-            yield return new WaitForSeconds(time);
-            shoulderCamera.transform.localPosition = new Vector3(0.0f, 0.0f, 0.0f);
-            shoulderCamera.transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
-            shoulderCamera.transform.SetParent(GameObject.Find("Global/Local VRRig/Local Gorilla Player/rig/body/head").transform, false);
-            set = true;
+            if (joyRC) 
+            {
+                shoulderCamera.transform.localPosition = ogvec;
+                cm.SetActive(true);
+                cam.fieldOfView = 120;
+
+            }
         }
     }
 }
